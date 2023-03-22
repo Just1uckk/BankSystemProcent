1b027229cfbc9db7bce5c72f4e9e903ea87d7032
nmBPLG4413wQ <horpynych_ap@groupbwt.com>
2023-03-22 13:09:05 +0200

    feat: add start function

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('HelloWorld!')
+const fs = require('fs')
+const path = require('path')
+const dotenv = require('dotenv')
+const { color, log, red, green } = require('console-log-colors');
+dotenv.config()
+
+let filename
+let mainDirectory = process.env.REPORT
+
+const start = async() => {
+    if (process.argv[2]) {
+        filename = process.argv[2]
+    } else {
+        filename = 'input.json'
+        console.log('Setting standart filename: input.json')
+    }
+
+    try {
+        let info = JSON.parse(fs.readFileSync(`${mainDirectory}${filename}`,'utf-8'))
+        console.log(green('JSON file info:'))
+        console.log(info)   
+    } catch (e) {
+        console.log(red('Invalid file name'))
+    }
+}
+start()
