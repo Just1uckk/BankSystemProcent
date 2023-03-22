af84a646a3099ea0f21000abaf5f3eeccf714da8
nmBPLG4413wQ <horpynych_ap@groupbwt.com>
2023-03-22 13:36:10 +0200

    fix: fetch module

--- app.js
+++ app.js
@@ -1,26 +1,8 @@
-const fs = require('fs')
-const path = require('path')
 const dotenv = require('dotenv')
-const { color, log, red, green } = require('console-log-colors');
 dotenv.config()
 
-let filename
-let mainDirectory = process.env.REPORT
+const {start,info} = require('./modules/startModule')
+const {fetchAPI,infoCashIn,infoCashOutLegal,infoCashOutNatural} = require('./modules/fetchModule')
 
-const start = async() => {
-    if (process.argv[2]) {
-        filename = process.argv[2]
-    } else {
-        filename = 'input.json'
-        console.log('Setting standart filename: input.json')
-    }
-
-    try {
-        let info = JSON.parse(fs.readFileSync(`${mainDirectory}${filename}`,'utf-8'))
-        console.log(green('JSON file info:'))
-        console.log(info)   
-    } catch (e) {
-        console.log(red('Invalid file name'))
-    }
-}
 start()
+fetchAPI()
